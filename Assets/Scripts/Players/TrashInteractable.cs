87ede42cb5b89cb183062abd0c7e332eb198040a
! ez.h <48311997+ezhhh@users.noreply.github.com>
2023-12-21 12:26:34 +0200

    grioujg

---
+++ Assets/Scripts/Players/TrashInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashInteractable : AbstractInteractable
+{
+    public override void Interact(LocalPlayer contextPlayer)
+    {
+        Destroy(gameObject);
+    }
+
+    public override string Title()
+    {
+        return "[E] Убрать мусор.";
+    }
+}
